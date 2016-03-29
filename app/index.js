961660c8e4f352b3c70626e1d9d90b16abf764b9
naemcivic <naemcivic@gmail.com>
2016-03-29 00:03:23 -0400

    complete building container component to render 3 child component

--- app/index.js
+++ app/index.js
@@ -1,15 +1,56 @@
+var USER_DATA = {
+  name: "React",
+  username: "naemcivic",
+  image: "https://avatars2.githubusercontent.com/u/15127278?v=3&s=460"
+}
+
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWord = React.createClass({
-  render: function () {
+var ProfilePic = React.createClass({
+  render: function() {
+    return  (<img src ={this.props.image} style={{heigh:100, width:100}} />)
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function(){
     return (
-      <div> This is react </div>
+        <div>
+          <a href={'https://www.github.com/' + this.props.username}>
+              {this.props.username}
+          </a>
+        </div>
+      )
+    }
+});
+
+var ProfileName = React.createClass({
+  render: function(){
+    return (
+      <div>{this.props.name}</div>
     )
   }
 });
 
+
+var Avatar = React.createClass ({
+  render: function(){
+    return (
+      <div>
+        <ProfilePic image={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+});
+
+
+
+
+
 ReactDom.render(
-  <HelloWord />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
-)
+);
