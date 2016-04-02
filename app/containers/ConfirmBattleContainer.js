a28bebf67e4b4b207973385383642e172bcdb9b4
naemcivic <naemcivic@gmail.com>
2016-04-02 12:28:57 -0400

    add helper method to fetch data from Github using Axios, wrote a promise to return each indivdual players data

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
-
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -14,24 +14,17 @@ var ConfirmBattleContainer = React.createClass({
     }
   },
 
-  componentWillMount: function () {
-    console.log('componentWillMount')
-  },
-
   componentDidMount: function () {
     var query = this.props.location.query;
     console.log('componentDidMount')
-    // Fetch info from github then update state
+      githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players){
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
-
-  componentWillReceiveProps: function () {
-    console.log('componentWillReceiveProps')
-  },
-
-  componentWillUnmount: function () {
-    console.log('componentWillUnmount');
-  },
-
 
   render: function () {
     return (
