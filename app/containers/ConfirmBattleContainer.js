9d96cae1e7b30e1dc634c7bc6f70577eeba28e3e
naemcivic <naemcivic@gmail.com>
2016-04-02 14:58:53 -0400

    Create additonal components to render UI

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -26,10 +26,19 @@ var ConfirmBattleContainer = React.createClass({
       }.bind(this))
   },
 
+handleInitiateBattle: function () {
+  this.context.router.push({
+    pathname: '/results',
+    state: {
+      playerInfo: this.state.playersInfo
+    }
+  })
+},
   render: function () {
     return (
       <ConfirmBattle
         isLoading={this.state.isLoading}
+        onInitiateBattle={this.handleInitiateBattle}
         playersInfo={this.state.playersInfo} />
     );
   }
